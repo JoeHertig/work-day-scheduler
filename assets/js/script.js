9087c84700cdb301d9383d73995725684a2e7ed4
Joe Hertig <joehertig94@gmail.com>
2021-08-29 16:48:35 -0600

    connected css and made first row to test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 // get current date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 // get current date end
+
+// edit text in textarea
+$(".description").on("click", "textarea", function () {
+  console.log(this);
+});
