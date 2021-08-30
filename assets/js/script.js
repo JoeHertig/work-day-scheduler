165e4b7b75d0338706cd3fce86dcc73ac7addfb4
Joe Hertig <joehertig94@gmail.com>
2021-08-29 21:42:27 -0600

    corrected rows so dotted line was in hour section, added Id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-//global variable
-var descriptions = {};
-
 // get current date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 // get current date end
@@ -13,7 +10,18 @@ $(".saveBtn").on("click", function () {
   // getting textarea value
   var btnText = $(this).siblings(".description").val();
   var btnTime = $(this).parent().attr("id");
+  console.log(this);
 
   // save to storage
   localStorage.setItem(btnText, btnTime);
 });
+
+$("#hour9 .description").val(localStorage.getItem("9"));
+$("#hour10 .description").val(localStorage.getItem("10"));
+$("#hour11 .description").val(localStorage.getItem("11"));
+$("#hour12 .description").val(localStorage.getItem("12"));
+$("#hour13 .description").val(localStorage.getItem("13"));
+$("#hour14 .description").val(localStorage.getItem("14"));
+$("#hour15 .description").val(localStorage.getItem("15"));
+$("#hour16 .description").val(localStorage.getItem("16"));
+$("#hour17 .description").val(localStorage.getItem("17"));
